d9ebfb599e28daff297dbf97c9386c131ecc9062
Cole Rau <colevrau@gmail.com>
2021-03-27 20:17:29 -0700

    bubble sort implemented

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,22 @@
 function bubbleSort(arr) {
-  console.log(arr)
-
+  let i = arr.length - 1
+  while (i >= 0) {
+    // console.log(i)
+    let j = 0
+    while (j < i) {
+      // console.log(j)
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1]
+        arr[j + 1] = arr[j]
+        arr[j] = temp
+      }
+      j++
+    }
+    i--
+  }
+  return arr
 }
 
 let arr = [4, 3, 6, 55, 2, 8, 67, 9, 0, 34, 2, 2, 1, -4, 3, 99, -2, -80, 101, 200, 100]
 console.log(bubbleSort(arr))
+// bubbleSort(arr)
