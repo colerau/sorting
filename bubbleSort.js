02ba355efd13c0f71e7f4151858259044c9cf4a4
Cole Rau <colevrau@gmail.com>
2021-03-27 20:30:55 -0700

    optimized bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,9 @@
 function bubbleSort(arr) {
+  let noSwaps
   let i = arr.length - 1
   while (i >= 0) {
     // console.log(i)
+    noSwaps = true
     let j = 0
     while (j < i) {
       // console.log(j)
@@ -9,14 +11,19 @@ function bubbleSort(arr) {
         let temp = arr[j + 1]
         arr[j + 1] = arr[j]
         arr[j] = temp
+        noSwaps = false
       }
       j++
+    }
+    if (noSwaps) {
+      break
     }
     i--
   }
   return arr
 }
 
-let arr = [4, 3, 6, 55, 2, 8, 67, 9, 0, 34, 2, 2, 1, -4, 3, 99, -2, -80, 101, 200, 100]
-console.log(bubbleSort(arr))
+// let arr = [4, 3, 6, 55, 2, 8, 67, 9, 0, 34, 2, 2, 1, -4, 3, 99, -2, -80, 101, 200, 100]
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 10, 9]
+console.log(bubbleSort(arr2))
 // bubbleSort(arr)
